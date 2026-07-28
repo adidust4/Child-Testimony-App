cf99b47a16eeca6b42204580c93101b7e51104c5
A'di Dust <dust@usc.edu>
2026-07-28 13:30:54 -0700

    fix static export

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,18 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
+
+const repo = "Child-Testimony-App";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.1.69'],
+  output: "export",
 
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+
+  allowedDevOrigins: ["192.168.1.69"],
 };
 
 export default nextConfig;
